0e8fa7405a7c9b06394cea71faf69f4e66076013
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-10-18 23:10:01 +0330

    ✨ feat: add chevron compact buttons components

---
+++ src/components/Buttons/ChevronCompactRight.jsx
@@ -0,0 +1,10 @@
+import ChevronCompactRightLogo from "assets/icons/chevron-compact-right.svg";
+const ChevronCompactRight = () => {
+  return (
+    <button type="button" className="arrow-key__right">
+      <img src={ChevronCompactRightLogo} alt="ChevronCompactRight SVG Logo" />
+    </button>
+  );
+};
+
+export default ChevronCompactRight;
