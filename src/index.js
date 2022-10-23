b8df7e773325e11406548ccb971c32b279f3d16d
Ali Bagheri <alibagheri20ab79@gmail.com>
2022-10-23 04:18:32 +0330

    � pref: clean up root folder structure

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import SSRProvider from "react-bootstrap/SSRProvider";
 
-import App from "./App";
-import Form from "./bootstrap/Form";
-import reportWebVitals from "./reportWebVitals";
+import App from "./containers/App";
+import UserInfo from "./containers/Bootstrap/UserForm.jsx";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,12 +11,7 @@ root.render(
   <React.StrictMode>
     <SSRProvider>
       <App />
-      <Form />
+      <UserInfo />
     </SSRProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
